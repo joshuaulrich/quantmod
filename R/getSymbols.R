1e04a75fd78a115cb3fa5a24edf17ce00ca3e2d3
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2007-07-03 04:35:55 +0000

    added saveSymbols
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/quantmod/trunk@38 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/getSymbols.R
+++ R/getSymbols.R
@@ -8,7 +8,7 @@ function(Symbols=NULL,
          verbose = FALSE,
          warnings = TRUE,
          src = c("MySQL","yahoo","google","economagic"),
-         user=NULL,password=NULL,dbname=NULL)  {
+         ...)  {
 
         if(reload.Symbols) {
             if(exists('.getSymbols',env,inherits=FALSE)) {
@@ -17,7 +17,6 @@ function(Symbols=NULL,
             }
         }
         removeSymbols(env=env)
-#this needs to be changed to accomodate additional methods - no partials??!!
       available.src = c("MySQL","yahoo","google","economagic")
       src = available.src[pmatch(src,available.src)][1]
 
@@ -26,15 +25,49 @@ function(Symbols=NULL,
         Symbols <- do.call(paste('getSymbols.',src,sep=''),list(Symbols=Symbols,env=env,db.fields=db.fields,
                                         field.names=field.names,reload.Symbols=reload.Symbols,
                                         verbose=verbose,warnings=warnings,
-                                        user=user,password=password,dbname=dbname))
+                                        ...))
 
         assign('.getSymbols',Symbols,env);
         invisible(return(env))
     } else {warning('no Symbols specified')}
 }
+"getSymbols.yahoo" <-
+function(Symbols,env,
+         from='1990-01-01',
+         to=Sys.Date(),
+         ...)
+{
+     yahoo.URL <- "http://chart.yahoo.com/table.csv?"
+     from.y <- as.numeric(strsplit(as.character(from),'-',)[[1]][1])
+     from.m <- as.numeric(strsplit(as.character(from),'-',)[[1]][2])-1
+     from.d <- as.numeric(strsplit(as.character(from),'-',)[[1]][3])
+     to.y <- as.numeric(strsplit(as.character(to),'-',)[[1]][1])
+     to.m <- as.numeric(strsplit(as.character(to),'-',)[[1]][2])-1
+     to.d <- as.numeric(strsplit(as.character(to),'-',)[[1]][3])
+     for(i in 1:length(Symbols)) {
+       fr <- read.csv(paste(yahoo.URL,
+                           "s=",Symbols[[i]],
+                           "&a=",from.m,
+                           "&b=",sprintf('%.2d',from.d),
+                           "&c=",from.y,
+                           "&d=",to.m,
+                           "&e=",sprintf('%.2d',to.d),
+                           "&f=",to.y,
+                           "&g=d&q=q&y=0",
+                           "&z=",Symbols[[i]],"&x=.csv",
+                           sep=''))
+       fr <- zoo(fr[,-1],as.Date(fr[,1]))
+       colnames(fr) <- paste(toupper(gsub('\\^','',Symbols[[i]])),
+                             c('Open','High','Low','Close','Volume','Adjusted'),
+                             sep='.')
+       class(fr) <- c("quantmod.OHLC","zoo")
+       assign(toupper(gsub('\\^','',Symbols[[i]])),fr,env)
+     }
+     return(Symbols)
+}
 "getSymbols.MySQL" <- function(Symbols,env,
                                db.fields=c('date','o','h','l','c','v','a'),
-                               return.type = c('quantmod.ohlc','data.frame'),
+                               return.type = c('quantmod.OHLC','zoo'),
                                field.names = NULL,
                                reload.Symbols = TRUE,
                                verbose = FALSE,
@@ -69,6 +102,7 @@ function(Symbols=NULL,
                                   c('Open','High','Low','Close','Volume','Adjusted'),
                                   sep='.')
             #class(fr) <- c("quantmod.OHLC","data.frame")
+            
             class(fr) <- c("quantmod.OHLC","zoo")
             assign(Symbols[[i]],fr,env)
             if(verbose) cat('done\n')
@@ -102,4 +136,22 @@ function(env=.GlobalEnv) {
     if(exists('.getSymbols',env,inherits=FALSE)) {
         return(unlist(get('.getSymbols',env)))
     } else { return(NULL) }
+}
+
+"saveSymbols"<-
+function(Symbols=NULL,file.path=stop("must specify file.path"),env=.GlobalEnv) {
+  if(exists('.getSymbols',env,inherits=FALSE)) {
+    getSymbols <- get('.getSymbols',env,inherits=FALSE)
+      if(is.null(Symbols)) {
+        Symbols <- paste(getSymbols)
+      } else {
+        #Symbols now has ONLY existing Symbols in it
+        Symbols <- Symbols[Symbols %in% unlist(getSymbols)]
+      }
+    for(each.symbol in Symbols) {
+      save(list=each.symbol,
+           file=paste(each.symbol,"RData",sep='.'),
+           env=env)
+    }    
+  }
 }
