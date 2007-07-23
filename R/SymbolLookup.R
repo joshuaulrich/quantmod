e2dfcaa2b28d476decbd028ea3ef88e373a91e1f
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2007-07-23 01:42:36 +0000

    moved most to Defaults
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/quantmod/trunk@98 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/SymbolLookup.R
@@ -0,0 +1,35 @@
+"setSymbolLookup" <-
+function(...)
+{
+  new.symbols <- list(...)
+  all.symbols <- getOption("getSymbols.sources")
+  for(each.symbol in names(new.symbols)) {
+    all.symbols[[each.symbol]] <- new.symbols[[each.symbol]]
+  }
+  options(getSymbols.sources=all.symbols)
+}
+
+"loadSymbolLookup" <-
+function(filename=".quantmod.SymbolSource.RData")
+{
+
+}
+
+"saveSymbolLookup" <-
+function(filename=".quantmod.SymbolSource.RData")
+{
+
+}
+
+"getSymbolLookup" <-
+function(Symbols=NULL)
+{
+  all.symbols <- getOption("getSymbols.sources")
+  if(is.null(Symbols)) Symbols <- names(all.symbols)
+  all.symbols[Symbols]
+}
+
+"unsetSymbolLookup" <-
+function(Symbol)
+{
+}
