ac48692f17be02b5b2a5d57ca07d7a4bf2834e3f
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2007-08-02 05:13:33 +0000

    don't knw
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/quantmod/trunk@117 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,9 @@
 ".onLoad" <- function(lib,pkg) {
+  cat("\n**WARNING**\n\n")
+  cat("This is currently 'quantmod' version 0.1-0")
+  cat("\nMany functions, features, and documentation are\n")
+  cat("incomplete. A new package will be available on CRAN\n")
+  cat("on or around useR2007 in Ames, IA, USA\n\n")
 #  require(methods,quietly=TRUE);
 #  require(zoo,quietly=TRUE);
 #  require(RMySQL,quietly=TRUE);
