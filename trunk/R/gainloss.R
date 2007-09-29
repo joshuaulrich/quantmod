c2c433e0c50159aae0d30c2ee41e9971c51fb791
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2007-09-29 16:22:03 +0000

    added some better params to plotting function - lots of work to do...

--- trunk/R/gainloss.R
+++ trunk/R/gainloss.R
@@ -1,13 +1,19 @@
-"gainloss" <- function(tsn) {
-  val.range <- sd(tsn[,1])*3
+"plotGainLoss" <- function(x,last.n.days=60) {
+  if(is.quantmodResults(x)) x <- modelSignal(x)
+  ms <- x[(NROW(x)-last.n.days):NROW(x),]
+  plwd <- 3
+  val.range <- sd(ms[,1])*3
   ylim <- c(-val.range,val.range)
   oldbg <- par('bg')
-  par(bg='#ffffff')
-  plot(tsn[,1],type='h',ylim=ylim,col='#dddddd',lwd=2,ylab='', xlab='')
-  par(new=T)
-  merged.d.f <- merge(abs(subset(tsn,tsn[,1]*tsn[,2]<0)[,1])*-1,abs(subset(tsn,tsn[,1]*tsn[,2]>0)[,1]))
-  plot(merged.d.f[,1],type='h',ylim=ylim,col='#ff0000',lwd=2,ylab='', xlab='')
-  par(new=T)
-  plot(merged.d.f[,2],type='h',ylim=ylim,col='#00ff00',lwd=2,ylab='', xlab='')
+  par(bg='#333333')
+  par(new=FALSE)
+  # plot all returns in bar style
+  plot(ms[,1],type='h',ylim=ylim,col='#dddddd',lwd=plwd,ylab='', xlab='',bty='n')
+  merged.d.f <- merge(abs(subset(ms,ms[,1]*ms[,2]<0)[,1])*-1,abs(subset(ms,ms[,1]*ms[,2]>0)[,1]))
+  par(new=TRUE)
+  plot(merged.d.f[,1],type='h',ylim=ylim,col='#ff0000',lwd=plwd,ylab='', xlab='')
+  par(new=TRUE)
+  plot(merged.d.f[,2],type='h',ylim=ylim,col='#00ff00',lwd=plwd,ylab='', xlab='')
+  grid(NA,5,lwd=1,col="#aaaaaa",lty=3)
   par(bg=oldbg)
 }
