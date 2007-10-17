20d10b9157c553e3eb1c5af8525073e19d4e7a03
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2007-10-17 02:22:48 +0000

    mods for documentation purposes

--- trunk/R/signtest.R
+++
@@ -1,7 +0,0 @@
-"sign.test" <-
-function(x,mu=0,p=.5) {
-  D <- length( (x-mu)[sign(x-mu) > 0] )
-  p.value <- sum(dbinom(D:length(x),length(x),p))
-  return(list("d-positive" = D,"p.value"=p.value))
-}
-
