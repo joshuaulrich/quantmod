7b1fe368959819dbaf5f703ff4dd2836e1d4760e
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2007-11-27 22:02:34 +0000

    chob wor cS2 temp work
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/quantmod/trunk@266 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/chob.R
+++ R/chob.R
@@ -8,6 +8,8 @@ function(x)
   locked <- bindingIsLocked('.chob',env)
   if(locked)
     unlockBinding('.chob',env)
+  orig.chob <- get.chob()
+  x <- c(orig.chob,x)
   assign('.chob',x,env)
   if(locked) {
     ow <- options("warn")
@@ -25,3 +27,54 @@ function()
   attr(x,'.Environment') <- NULL
   return(x)
 }
+
+`release.chob` <-
+function(n)
+{
+  if(missing(n)) {
+    x <- list()  
+  } else {
+    x <- get.chob()[-n]
+  }
+  env <- as.environment("package:quantmod")
+  environment(x) <- env
+  locked <- bindingIsLocked('.chob',env)
+  if(locked)
+    unlockBinding('.chob',env)
+  assign('.chob',x,env)
+  if(locked) {
+    ow <- options("warn")
+    on.exit(options(ow))
+    options(warn=-1)
+    lockBinding('.chob',env)
+   }
+  invisible(1)
+}
+
+
+`print.chob` <-
+function(x)
+{
+#  plot.chob(x)
+}
+setClass("chob",
+         representation(
+           device="ANY",
+           call="call",
+           passed.args="ANY",
+           windows="numeric",
+           xrange="numeric",
+           yrange="numeric",
+           length="numeric"
+         )
+)
+
+setClass("chobTA",
+         representation(
+            new="logical",
+            TA.values="ANY",
+            name="character"
+         )
+)
+
+##setMethod("show","chob",function(object) { plot.chob(object) })
