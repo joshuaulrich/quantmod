9903d72d49871755460f40f4c4e4bf2f13f94036
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2008-05-21 03:53:13 +0000

    new getQuote formatting getDividends bug in date specification - thanks to David Reiner from Rho for pointing that out Delt now uses try.xts/reclass
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/quantmod/pkg@412 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/getQuote.R
+++ R/getQuote.R
@@ -43,7 +43,7 @@
 #}
 
 `getQuote` <-
-function(Symbols,src='yahoo',what='d1t1l1c1ohgv') {
+function(Symbols,src='yahoo',what='nd1t1l1c1p2ohgv') {
   if(src != 'yahoo') stop('no additional src methods available yet')
   tmp <- tempfile()
   Symbols <- paste(strsplit(Symbols,';')[[1]],collapse="+")
@@ -55,14 +55,53 @@ function(Symbols,src='yahoo',what='d1t1l1c1ohgv') {
   sq <- read.csv(file=tmp,sep=',',stringsAsFactors=FALSE,header=FALSE)
   unlink(tmp)
   Qposix <- strptime(paste(sq[,1],sq[,2]),format='%m/%d/%Y %H:%M')
-  #Qposix <- (paste(sub('(.+)/(.+)/(....)','\\3/\\1/\\2',sq[,3]),
-  #                           sq[,4]))
-  #class(Qposix) <- c("POSIXt","POSIXct")
-  #Q.xts <- xts(sq[,c(2,5,6,7,8,9)],Qposix)
-  #dim(Q.xts) <- c(1,6)
-  #if(what=='d1t1l1c1ohgv')
-  #  colnames(Q.xts) <- paste(Symbols,c("Last","Change","Open","High","Low","Volume"),sep=".")
-  #Q.xts
-  data.frame(Qposix,sq[,3:NCOL(sq)])
+  Symbols <- unlist(strsplit(Symbols,'\\+'))
+  df <- data.frame(Qposix,sq[,3:NCOL(sq)])
+  rownames(df) <- Symbols
+  if(what=='nd1t1l1c1p2ohgv') {
+    colnames(df) <- c('Name','Trade Time','Last','Change','% Change','Open','High','Low','Volume')
+  }
+  df
 }
 
+`yahooQuoteFormat` <- function() {
+   optnames <- c("Ask", "Average Daily Volume", "Ask Size", "Bid", "Ask (Real-time)", 
+              "Bid (Real-time)", "Book Value", "Bid Size", "Change & Percent Change", 
+              "Change", "Commission", "Change (Real-time)", "After Hours Change (Real-time)", 
+              "Dividend/Share", "Last Trade Date", "Trade Date", "Earnings/Share", 
+              "Error Indication (returned for symbol changed / invalid)", "EPS Estimate Current Year", 
+              "EPS Estimate Next Year", "EPS Estimate Next Quarter", "Float Shares", 
+              "Days Low,h,Days High", "52-week Low", "52-week High", "Holdings Gain Percent", 
+              "Annualized Gain", "Holdings Gain", "Holdings Gain Percent (Real-time)", 
+              "Holdings Gain (Real-time)", "More Info", "Order Book (Real-time)", 
+              "Market Capitalization", "Market Cap (Real-time)", "EBITDA", 
+              "Change From 52-week Low", "Percent Change From 52-week Low", 
+              "Last Trade (Real-time) With Time", "Change Percent (Real-time)", 
+              "Last Trade Size", "Change From 52-week High", "Percent Change From 52-week High", 
+              "Last Trade (With Time)", "Last Trade (Price Only)", "High Limit", 
+              "Low Limit", "Days Range","Days Range (Real-time)", "50-day Moving Average", 
+              "200-day Moving Average", "Change From 200-day Moving Average", 
+              "Percent Change From 200-day Moving Average", "Change From 50-day Moving Average", 
+              "Percent Change From 50-day Moving Average", "Name", "Notes", 
+              "Open", "Previous Close", "Price Paid", "Change in Percent", 
+              "Price/Sales", "Price/Book", "Ex-Dividend Date", "P/E Ratio", 
+              "Dividend Pay Date", "P/E Ratio (Real-time)", 
+              "PEG Ratio", "Price/EPS Estimate Current Year", 
+              "Price/EPS Estimate Next Year", "Symbol", "Shares Owned", "Short Ratio", 
+              "Last Trade Time", "Trade Links", "Ticker Trend", "1 yr Target Price", 
+              "Volume", "Holdings Value", "Holdings Value (Real-time)", "52-week Range", 
+              "Days Value Change", "Days Value Change (Real-time)", "Stock Exchange", 
+              "Dividend Yield")
+  optcodes <- c("a", "a2", "a5", "b", "b2", "b3", "b4", "b6", "c", "c1", "c3", 
+              "c6", "c8", "d", "d1", "d2", "e", "e1", "e7", "e8", "e9", "f6", 
+              "g", "j", "k", "g1", "g3", "g4", "g5", "g6", "i", "i5", "j1", 
+              "j3", "j4", "j5", "j6", "k1", "k2", "k3", "k4", "k5", "l", "l1", 
+              "l2", "l3", "m", "m2", "m3", "m4", "m5", "m6", "m7", "m8", "n", "n4", 
+              "o", "p", "p1", "p2", "p5", "p6", "q", "r", "r1", "r2", "r5", 
+              "r6", "r7", "s", "s1", "s7", "t1", "t6", "t7", "t8", "v", "v1", 
+              "v7", "w", "w1", "w4", "x", "y")
+  w <- which(optnames %in% select.list(optnames, multiple=TRUE))
+  str <- paste(optcodes[w],collapse='')
+  nms <- optnames[w]
+  return(structure(list(str,nms),class='yahooQuoteFormat'))
+}
