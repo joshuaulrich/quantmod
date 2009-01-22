5b9c2b5765cba60beaec87bfc11271f61a3a81e1
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2009-01-22 15:36:40 +0000

    new attachSymbols included in svn peak changed to findPeaks and valley to findValleys
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/quantmod/pkg@478 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/peak.R
+++ R/peak.R
@@ -1,10 +1,25 @@
-`peak` <-
-function(x) {
-  which(diff(sign(diff(x, na.pad=FALSE)),na.pad=FALSE) < 0) + 2
+findPeaks <-
+function(x, thresh=0) {
+  pks <- which(diff(sign(diff(x, na.pad=FALSE)),na.pad=FALSE) < 0) + 2
+  if( !missing(thresh) ) {
+   pks[x[pks-1]-x[pks] > thresh]
+  } else pks
 }
 
-`valley` <-
-function(x) {
-  which(diff(sign(diff(x, na.pad=FALSE)),na.pad=FALSE) > 0) + 2
+peak <- function(x) {
+  .Deprecated("findPeaks", package="quantmod")
+  findPeaks(x)
 }
 
+findValleys <-
+function(x, thresh=0) {
+  pks <- which(diff(sign(diff(x, na.pad=FALSE)),na.pad=FALSE) > 0) + 2
+  if( !missing(thresh) ) {
+   pks[x[pks-1]-x[pks] > thresh]
+  } else pks
+}
+
+valley <- function(x) {
+  .Deprecated("findValleys", package="quantmod")
+  findValleys(x)
+}
