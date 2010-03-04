9366004d8626c6683a5dbc33eb0eefb21bda2b58
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2010-03-04 16:21:19 +0000

    o  add_axis to add aribitrarily spaced ticks on left    or right y axis o  experimental add_Last (unexported) to append final    price values and names to RHS of chart

---
+++ pkg/R/add_Last.R
@@ -0,0 +1,81 @@
+add_Last <-
+function(name=TRUE,last=TRUE) {
+  lenv <- new.env()
+  lenv$plot_axis <- function(x,show.last,show.name) {
+    xdata <- x$Env$xdata
+    nr <- NROW(x$Env$xdata[x$Env$xsubset])
+    last_values <- last(xdata[x$Env$xsubset])
+    offset <- offset_names <- NULL
+    offsets <- NULL
+    offset <- max(strwidth(pretty(as.numeric(last_values))))
+    if(!x$Env$rylab)
+      offset <- 0
+    offset_names <- max(strwidth(colnames(last_values)))  # only relevant to names & last
+    for(i in 1:NCOL(last_values)) {
+      if(!name) {
+        text(nr+offset, as.numeric(last_values[,i]),
+             sprintf("%+.2f",last_values[,i]), col=x$Env$theme$line.col[i],
+             cex=.9,font=2,pos=4,xpd=TRUE)
+      } else if(!last) {
+        text(nr+offset, as.numeric(last_values[,i]),
+             colnames(last_values)[i], col=x$Env$theme$line.col[i],
+             cex=.9,font=2,pos=4,xpd=TRUE)
+      } else {
+        text((nr+offset)+c(0,offset_names), as.numeric(last_values[,i]),
+             c(colnames(last_values)[i],sprintf("%+.2f",last_values[,i])),
+             col=c(x$Env$theme$line.col[i],'black'),
+             cex=.9,font=c(2,1),pos=4,xpd=TRUE)
+      }
+    }
+  }
+  #exp <- expression(plot_axis(x=current.chob(),show.last=last,show.name=name))
+  exp <- parse(text = gsub("list", "plot_axis", as.expression(substitute(list(x = current.chob(), 
+               show.last=last, show.name=name,...)))), srcfile = NULL)
+  plot_object <- quantmod:::current.chob()
+browser()
+  plot_object$Env$mar <- c(3,1,0,if(name & last) if(plot_object$Env$rylab) 5 else 4 else 3)
+  lenv$xdata <- plot_object$Env$xdata
+  plot_object$set_frame(2,clip=FALSE)
+  plot_object$add(exp, env=c(lenv,plot_object$Env), expr=TRUE, clip=FALSE)
+  plot_object
+}
+
+add_axis <-
+function(side, at=NULL, labels=TRUE, tick=TRUE, line=NA, pos=NA, font=NA, col=NULL) {
+  lenv <- new.env()
+  lenv$plot_axis <- function(x,side,at,labels,font,pos,col) {
+    xdata <- x$Env$xdata
+    if(is.OHLC(xdata))
+      xdata <- OHLC(xdata)
+    xsubset <- x$Env$xsubset
+    nr <- NROW(x$Env$xdata[x$Env$xsubset])
+    if(is.logical(labels) && labels==TRUE) {
+      labels <- pretty(xdata[xsubset])
+      dropped_label <- which(labels < min(xdata[xsubset],na.rm=TRUE))
+      labels <- labels[-dropped_label]
+    } 
+    if(is.null(at))
+      at <- labels
+    if(side==2) {
+      nr <- 0
+    }
+    text(nr, at, labels, col=col,
+         cex=.9,font=font,pos=pos,xpd=TRUE)
+    segments(nr-(1/8 * max(strwidth(labels))),at,
+             nr+(1/8 * max(strwidth(labels))),at)
+  }
+  if(missing(pos))
+    pos <- side
+  mapply(function(name, value) {
+      assign(name, value, envir = lenv)
+  }, names(list(side=side,at=at,labels=labels,font=font,pos=pos,col=col)),
+     list(side=side,at=at,labels=labels,font=font,pos=pos,col=col))
+  exp <- parse(text = gsub("list", "plot_axis", as.expression(substitute(list(x = current.chob(), 
+               side=side, at=get("at"), labels=get("labels"), font=font,pos=pos, col=col)))), srcfile = NULL)
+  plot_object <- quantmod:::current.chob()
+  lenv$xdata <- plot_object$Env$xdata
+  plot_object$set_frame(2)
+  plot_object$add(exp, env=c(lenv,plot_object$Env), expr=TRUE)
+  plot_object
+}
+
