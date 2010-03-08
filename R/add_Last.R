f42adbaf07382bcc857bfc78c2c9644d38bba5b2
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2010-03-08 18:29:54 +0000

    o  fixed bug in chart_Series when drawing series with embedded NAs o  add_title skeleton
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/quantmod/pkg@540 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/add_Last.R
+++ R/add_Last.R
@@ -32,7 +32,6 @@ function(name=TRUE,last=TRUE) {
   exp <- parse(text = gsub("list", "plot_axis", as.expression(substitute(list(x = current.chob(), 
                show.last=last, show.name=name,...)))), srcfile = NULL)
   plot_object <- quantmod:::current.chob()
-browser()
   plot_object$Env$mar <- c(3,1,0,if(name & last) if(plot_object$Env$rylab) 5 else 4 else 3)
   lenv$xdata <- plot_object$Env$xdata
   plot_object$set_frame(2,clip=FALSE)
@@ -43,7 +42,48 @@ browser()
 add_axis <-
 function(side, at=NULL, labels=TRUE, tick=TRUE, line=NA, pos=NA, font=NA, col=NULL) {
   lenv <- new.env()
-  lenv$plot_axis <- function(x,side,at,labels,font,pos,col) {
+  lenv$plot_axis <- function(x,side,at,labels,tick,font,pos,col) {
+    xdata <- x$Env$xdata
+    if(is.OHLC(xdata))
+      xdata <- OHLC(xdata)
+    xsubset <- x$Env$xsubset
+    nr <- NROW(x$Env$xdata[x$Env$xsubset])
+    if(is.logical(labels) && labels==TRUE) {
+      labels <- pretty(xdata[xsubset])
+      dropped_label <- which(labels < min(xdata[xsubset],na.rm=TRUE))
+      labels <- labels[-dropped_label]
+    } 
+    if(is.null(at))
+      at <- labels
+    if(side==2) {
+      nr <- 0
+    }
+    text(nr, at, labels, col=col,
+         cex=.9,font=font,pos=pos,xpd=TRUE)
+    if(tick)
+      segments(nr-(1/8 * max(strwidth(labels))),at,
+               nr+(1/8 * max(strwidth(labels))),at)
+  }
+  if(missing(pos))
+    pos <- side
+  mapply(function(name, value) {
+      assign(name, value, envir = lenv)
+  }, names(list(side=side,at=at,labels=labels,font=font,tick=tick,pos=pos,col=col)),
+     list(side=side,at=at,labels=labels,font=font,tick=tick,pos=pos,col=col))
+  exp <- parse(text = gsub("list", "plot_axis", as.expression(substitute(list(x = current.chob(), 
+               side=side, at=get("at"), labels=get("labels"), tick=tick,
+               font=font,pos=pos, col=col)))), srcfile = NULL)
+  plot_object <- quantmod:::current.chob()
+  lenv$xdata <- plot_object$Env$xdata
+  plot_object$set_frame(2)
+  plot_object$add(exp, env=c(lenv,plot_object$Env), expr=TRUE)
+  plot_object
+}
+
+add_title <-
+function(main=NULL, sub=NULL, xlab=NULL, ylab=NULL, line=NA, ...) {
+  lenv <- new.env()
+  lenv$plot_title <- function(x,main,sub,xlab,ylab,line) {
     xdata <- x$Env$xdata
     if(is.OHLC(xdata))
       xdata <- OHLC(xdata)
@@ -78,4 +118,3 @@ function(side, at=NULL, labels=TRUE, tick=TRUE, line=NA, pos=NA, font=NA, col=NU
   plot_object$add(exp, env=c(lenv,plot_object$Env), expr=TRUE)
   plot_object
 }
-
