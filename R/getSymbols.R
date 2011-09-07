17a0436302e871551bb2bc07cf2d0051bb778275
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2011-09-07 17:08:50 +0000

    o getSymbols.Bloomberg contributed code now available for testing (undocumented)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/quantmod/pkg@567 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/getSymbols.R
+++ R/getSymbols.R
@@ -89,6 +89,102 @@ loadSymbols.formals$env <- substitute(.GlobalEnv)
 formals(loadSymbols) <- loadSymbols.formals
 
 
+# getSymbols.Bloomberg {{{
+"getSymbols.Bloomberg" <- function(Symbols,env,return.class='xts',
+                              from=as.POSIXlt(Sys.time()-60*60,"GMT"),
+                              to=as.POSIXlt(Sys.time(),"GMT"),
+                              bb.suffix="Equity",
+                              bb.interval="5",
+                              ...) {
+    importDefaults("getSymbols.Bloomberg")
+    this.env <- environment()
+    for(var in names(list(...))) {
+       # import all named elements that are NON formals
+       assign(var, list(...)[[var]], this.env)
+    }
+    if ((class(from)=="Date" && class(to)=="Date") ||
+           (class(from)=="character" && length(from)<=8 &&
+               class(to)=="character" && length(to)<=8 )) {
+       bb.intraday <- FALSE
+       bb.call <- bdh
+       bb.fields <- c("OPEN", "HIGH", "LOW", "PX_LAST", "VOLUME")
+    } else {
+       bb.intraday <- TRUE
+       bb.call <- bar
+       bb.fields <- "TRADE"
+    }
+    if(missing(verbose)) verbose <- FALSE
+    if(missing(auto.assign)) auto.assign <- TRUE
+       if('package:RBloomberg' %in% search() ||
+require('RBloomberg',quietly=TRUE)) {
+         {}
+       } else {
+         stop(paste("package:",dQuote('RBloomberg'),"cannot be loaded."))
+       }
+       bbconn <- blpConnect()
+       for(i in 1:length(Symbols)) {
+           bbsym <- paste(Symbols[[i]],bb.suffix)
+
+           if(verbose) {
+               cat(paste('Loading ',bbsym, ' from BB ', from,' to ',to,
+                   paste(rep('.',18-nchar(Symbols[[i]])),collapse=''),
+                   sep=''))
+
+           }
+           tryCatch (
+             {
+               if (bb.intraday) {
+                 fromStr <- paste(as.character(from),".000",sep="")
+                 toStr <- paste(as.character(to),".000",sep="")
+                 b <- bb.call(bbconn, bbsym, bb.fields,
+                             fromStr, toStr, bb.interval)
+                 b$datetime <- as.POSIXct(strptime(b$time,
+format="%Y-%m-%dT%H:%M:%S"))
+                 bxo <- as.xts(b$open, order.by=b$datetime)
+                 fr <- merge(bxo,  b$high, b$low, b$close, b$volume)
+               } else {
+                 if (class(from)=="character") {
+                   fromStr <- from
+                 } else {
+                   fromStr <- strftime(from,format="%Y%m%d")
+                 }
+                 if (class(to)=="character") {
+                   toStr <- to
+                 } else {
+                   toStr <- strftime(to,format="%Y%m%d")
+                 }
+                 b <- bb.call(bbconn, bbsym, bb.fields,
+                             fromStr, toStr)
+                 b$datetime <- as.POSIXct(strptime(b$date,
+format="%Y-%m-%d"))
+                 bxo <- as.xts(b$OPEN, order.by=b$datetime)
+                 fr <- merge(bxo,  b$HIGH, b$LOW, b$PX_LAST, b$VOLUME)
+               }
+
+
+
+               if(verbose) {
+                 cat(paste(length(fr),'points '))
+               }
+               colnames(fr) <- paste(Symbols[[i]],
+                                     c('Open','High','Low','Close','Volume'),
+                                     sep='.')
+               fr <- convert.time.series(fr=fr,return.class=return.class)
+               if(auto.assign)
+                 assign(Symbols[[i]],fr,env)
+             },
+             error=function(e) {print(e);fr <- data.frame()},
+             finally=function () {if(verbose) {cat('done\n')}}
+           )
+       }
+       blpDisconnect(bbconn)
+       if(auto.assign)
+         return(Symbols)
+       return(fr)
+}
+"getSymbols.Bloomberg" <- getSymbols.Bloomberg
+# }}}
+
 # getSymbols.yahoo {{{
 "getSymbols.yahoo" <-
 function(Symbols,env,return.class='xts',index.class="Date",
