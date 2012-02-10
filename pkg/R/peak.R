a89d6ab409f728a337c24096cede36bb1a2227eb
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2012-02-10 03:38:44 +0000

    o  fixed another one of Josh's bugs ;-)  The even show up in code he doesn't write...

--- pkg/R/peak.R
+++ pkg/R/peak.R
@@ -2,7 +2,7 @@ findPeaks <-
 function(x, thresh=0) {
   pks <- which(diff(sign(diff(x, na.pad=FALSE)),na.pad=FALSE) < 0) + 2
   if( !missing(thresh) ) {
-   pks[x[pks-1]-x[pks] > thresh]
+   pks[x[pks-1]-coredata(x[pks]) > thresh]
   } else pks
 }
 
@@ -15,7 +15,7 @@ findValleys <-
 function(x, thresh=0) {
   pks <- which(diff(sign(diff(x, na.pad=FALSE)),na.pad=FALSE) > 0) + 2
   if( !missing(thresh) ) {
-   pks[x[pks-1]-x[pks] > thresh]
+   pks[x[pks-1]-coredata(x[pks]) > thresh]
   } else pks
 }
 
