3b3ecfd76eae56531d4dbb0f991ea8fb6c07581c
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2012-12-19 03:44:39 +0000

    add_VMA and add_EVMA

--- pkg/R/chart_Series.R
+++ pkg/R/chart_Series.R
@@ -640,6 +640,24 @@ add_WMA <- function(n=10, wts=1:n, on=1, col='green',...) {
   plot_object$add(exp,env=c(lenv, plot_object$Env),expr=TRUE)
   plot_object
 } # }}}
+# add_VMA {{{
+add_VMA <- function(w, ratio=1, on=1, col='green',...) {
+  lenv <- new.env()
+  lenv$add_wma <- function(x, w, ratio, col, ...) {
+    xdata <- x$Env$xdata
+    xsubset <- x$Env$xsubset
+    vma <- VMA(Cl(xdata), w=w, ratio=ratio)[xsubset]
+    lines(1:NROW(xdata[xsubset]), vma, col=col, ...)
+  }
+  mapply(function(name,value) { assign(name,value,envir=lenv) }, names(list(w=w,ratio=ratio,col=col,...)), list(w=w,ratio=ratio,col=col,...))
+  exp <- parse(text=gsub("list","add_wma",as.expression(substitute(list(x=current.chob(),w=w,ratio=ratio,col=col,...)))),
+               srcfile=NULL)
+  plot_object <- current.chob()
+  lenv$xdata <- VMA(Cl(plot_object$Env$xdata),w=w,ratio=ratio)
+  plot_object$set_frame(sign(on)*(abs(on)+1L))
+  plot_object$add(exp,env=c(lenv, plot_object$Env),expr=TRUE)
+  plot_object
+} # }}}
 # add_DEMA {{{
 add_DEMA <- function(n=10, on=1, col='pink', ...) {
   lenv <- new.env()
@@ -673,6 +691,25 @@ add_VWAP <- function(n=10, on=1, col='darkgrey', ...) {
                srcfile=NULL)
   plot_object <- current.chob()
   lenv$xdata <- VWAP(Cl(plot_object$Env$xdata),plot_object$Env$vo,n=n)
+  plot_object$set_frame(sign(on)*(abs(on)+1L))
+  plot_object$add(exp,env=c(lenv, plot_object$Env),expr=TRUE)
+  plot_object
+} # }}}
+# add_EVWMA {{{
+add_EVWMA <- function(n=10, on=1, col='darkgrey', ...) {
+  lenv <- new.env()
+  lenv$add_evwma <- function(x, n, col, ...) {
+    xdata <- x$Env$xdata
+    xvo <- x$Env$vo
+    xsubset <- x$Env$xsubset
+    evwma <- EVWMA(Cl(xdata),xvo, n=n)[xsubset]
+    lines(1:NROW(xdata[xsubset]), evwma, col=col, ...)
+  }
+  mapply(function(name,value) { assign(name,value,envir=lenv) }, names(list(n=n,col=col,...)), list(n=n,col=col,...))
+  exp <- parse(text=gsub("list","add_evwma",as.expression(substitute(list(x=current.chob(),n=n,col=col,...)))),
+               srcfile=NULL)
+  plot_object <- current.chob()
+  lenv$xdata <- EVWMA(Cl(plot_object$Env$xdata),plot_object$Env$vo,n=n)
   plot_object$set_frame(sign(on)*(abs(on)+1L))
   plot_object$add(exp,env=c(lenv, plot_object$Env),expr=TRUE)
   plot_object
