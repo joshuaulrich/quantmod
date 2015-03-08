d6bd4d6c6102f57dd54bb29f7f1d1aee1606c087
Joshua Ulrich <josh.m.ulrich@gmail.com>
2015-03-08 12:47:16 -0500

    Handle expiry dates in getOptionChain.yahoo
    Add functionality to request expiry dates, and return all expiries if
    Exp=NULL. Also add ability to specify expiry date as ISO-8601 style
    string. This completes the patch started in r625, and fixes #27.

--- R/getOptionChain.R
+++ R/getOptionChain.R
@@ -10,8 +10,8 @@ function(Symbols, Exp=NULL, src="yahoo", ...) {
 
 getOptionChain.yahoo <- function(Symbols, Exp, ...)
 {
-  if(!require(XML, quietly=TRUE))
-    stop("package:",dQuote("XML"),"cannot be found.")
+  if(!requireNamespace("XML", quietly=TRUE))
+    stop("package:",dQuote("XML"),"cannot be loaded.")
 
   thParse <- function(x) {
     if (length(XML::xmlChildren(x)) > 1) {
@@ -26,21 +26,71 @@ getOptionChain.yahoo <- function(Symbols, Exp, ...)
     d[] <- lapply(d, type.convert, as.is=TRUE)
     d
   }
-  tbl <- XML::htmlParse(paste0("http://finance.yahoo.com/q/op?s=", Symbols[1], "&size=mini"), isURL=TRUE)
 
+  # Don't check the expiry date if we're looping over dates we just scraped
+  checkExp <- !hasArg(".expiry.known") || !match.call(expand.dots=TRUE)$.expiry.known
+  # Construct URL
+  urlExp <- paste0("http://finance.yahoo.com/q/op?s=", Symbols[1])
+  # Add expiry date to URL
+  if(!checkExp)
+    urlExp <- paste0(urlExp, "&date=", Exp)
+
+  # Fetch data; ensure object is free'd on function exit
+  tbl <- XML::htmlParse(urlExp, isURL=TRUE)
+  on.exit(XML::free(tbl))
+
+  # xpaths to the data we're interested in
   xpaths <- list()
   xpaths$tables <- "//table[contains(@class, 'quote-table')]"
   xpaths$table.names <- paste0(xpaths$tables, "/caption/text()")
   xpaths$headers <- paste0(xpaths$tables, "/thead/tr[not(contains(@class, 'filterRangeRow'))]")
+  xpaths$expiries <- "//div[contains(@class, 'options_menu')]/form/select//option"
 
+  # Extract table names and headers
   table.names <- XML::xpathSApply(tbl, xpaths$table.names, XML::xmlValue)
   table.names <- tolower(gsub("[[:space:]]", "", table.names))
   table.headers <- XML::xpathApply(tbl, xpaths$headers, fun=function(x) sapply(x['th'], thParse))
 
+  # Only return nearest expiry (default served by Yahoo Finance), unless the user specified Exp
+  if(!missing(Exp) && checkExp) {
+    all.expiries <- XML::xpathSApply(tbl, xpaths$expiries, XML::xmlGetAttr, name="value")
+    all.expiries.posix <- .POSIXct(as.numeric(all.expiries), tz="UTC")
+
+    if(is.null(Exp)) {
+      # Return all expiries if Exp = NULL
+      out <- lapply(all.expiries, getOptionChain.yahoo, Symbols=Symbols, .expiry.known=TRUE)
+      # Expiry format was "%b %Y", but that's not unique with weeklies. Change
+      # format to "%b.%d.%Y" ("%Y-%m-%d wouldn't be good, since names should
+      # start with a letter or dot--naming things is hard).
+      return(setNames(out, format(all.expiries.posix, "%b.%d.%Y")))
+    } else {
+      # Ensure data exist for user-provided expiry date(s)
+      if(inherits(Exp, "Date"))
+        valid.expiries <- as.Date(all.expiries.posix) %in% Exp
+      else if(inherits(Exp, "POSIXt"))
+        valid.expiries <- all.expiries.posix %in% Exp
+      else if(is.character(Exp)) {
+        expiry.range <- range(unlist(lapply(Exp, .parseISO8601, tz="UTC")))
+        valid.expiries <- all.expiries.posix >= expiry.range[1] &
+                          all.expiries.posix <= expiry.range[2]
+      }
+      if(all(!valid.expiries))
+        stop("Provided expiry date(s) not found. Available dates are: ",
+             paste(as.Date(all.expiries.posix), collapse=", "))
+
+      expiry.subset <- all.expiries[valid.expiries]
+      if(length(expiry.subset) == 1)
+        return(getOptionChain.yahoo(Symbols, expiry.subset, .expiry.known=TRUE))
+      else {
+        out <- lapply(expiry.subset, getOptionChain.yahoo, Symbols=Symbols, .expiry.known=TRUE)
+        # See comment above regarding the output names
+        return(setNames(out, format(all.expiries.posix[valid.expiries], "%b.%d.%Y")))
+      }
+    }
+  }
+
   dftables <- XML::xmlApply(XML::getNodeSet(tbl, xpaths$tables), XML::readHTMLTable, stringsAsFactors=FALSE)
   names(dftables) <- table.names
-
-  XML::free(tbl)
 
   dftables <- mapply(setNames, dftables, table.headers, SIMPLIFY=FALSE)
   dftables <- lapply(dftables, NewToOld)
