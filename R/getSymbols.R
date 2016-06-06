ce7a07ea3bdaeb28e90a55361c3e6130bd2c8d6d
AndreMikulec <Andre_Mikulec@Hotmail.com>
2016-06-05 19:08:00 -0500

    added getSymbols.PostgreSQL

--- R/getSymbols.R
+++ R/getSymbols.R
@@ -654,6 +654,71 @@ function(Symbols,env,return.class='xts',
 "getSymbols.mysql" <- getSymbols.MySQL
 # }}}
 
+# getSymbols.PostgreSQL {{{
+"getSymbols.PostgreSQL" <- function(Symbols,env,return.class='xts',
+                               db.fields=c('date','o','h','l','c','v','a'),
+                               field.names = NULL,
+                               user=NULL,password=NULL,dbname=NULL,host='localhost',port=5432,options="",search_path=NULL,
+                               ...) {
+     importDefaults("getSymbols.PostgreSQL")
+     this.env <- environment()
+     for(var in names(list(...))) {
+        # import all named elements that are NON formals
+        assign(var, list(...)[[var]], this.env)
+     }
+     if(!hasArg(verbose)) verbose <- FALSE
+     if(!hasArg(auto.assign)) auto.assign <- TRUE
+
+     if(!requireNamespace("DBI", quietly=TRUE))
+       stop("package:",dQuote("DBI"),"cannot be loaded.")
+     if(!requireNamespace("RPostgreSQL", quietly=TRUE))
+       stop("package:",dQuote("RPostgreSQL"),"cannot be loaded.")
+
+        if(is.null(user) || is.null(password) || is.null(dbname)) {
+          stop(paste(
+              'At least one connection argument (',sQuote('user'),
+              sQuote('password'),sQuote('dbname'),
+              ") is not set"))
+        }
+        con <- DBI::dbConnect(RPostgreSQL::PostgreSQL(),user=user,password=password,dbname=dbname,host=host,port=port,options=options)
+
+        if(!is.null(search_path)) { 
+          dbGetQuery(con, paste0("set search_path to ", search_path) )
+        }
+
+        db.Symbols <- DBI::dbListTables(con)
+        if(length(Symbols) != sum(tolower(Symbols) %in% db.Symbols)) {
+          missing.db.symbol <- Symbols[!tolower(Symbols) %in% db.Symbols]
+                warning(paste('could not load symbol(s): ',paste(missing.db.symbol,collapse=', ')))
+                Symbols <- Symbols[tolower(Symbols) %in% db.Symbols]
+        }
+        for(i in seq_along(Symbols)) {
+            if(verbose) {
+                cat(paste('Loading ',Symbols[[i]],paste(rep('.',10-nchar(Symbols[[i]])),collapse=''),sep=''))
+            }
+            query <- paste("SELECT ",paste(db.fields,collapse=',')," FROM ",Symbols[[i]]," ORDER BY date")
+            rs <- DBI::dbSendQuery(con, query)
+            fr <- DBI::fetch(rs, n=-1)
+            #fr <- data.frame(fr[,-1],row.names=fr[,1])
+            fr <- xts(as.matrix(fr[,-1]),
+                      order.by=as.Date(fr[,1],origin='1970-01-01'),
+                      src=dbname,updated=Sys.time())
+            colnames(fr) <- paste(Symbols[[i]],
+                                  c('Open','High','Low','Close','Volume','Adjusted'),
+                                  sep='.')
+            fr <- convert.time.series(fr=fr,return.class=return.class)
+            if(auto.assign)
+              assign(Symbols[[i]],fr,env)
+            if(verbose) cat('done\n')
+        }
+        DBI::dbDisconnect(con)
+        if(auto.assign)
+          return(Symbols)
+        return(fr)
+}
+"getSymbols.PostgreSQL" <- getSymbols.PostgreSQL 
+# }}}
+
 # getSymbols.FRED {{{
 `getSymbols.FRED` <- function(Symbols,env,
      return.class="xts", ...) {
