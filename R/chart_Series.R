427e68a6c2f1997ce477f2bacd3ed54913b585fe
Joshua Ulrich <josh.m.ulrich@gmail.com>
2016-12-14 17:43:38 -0600

    Evaluate TA in parent frame of chart_Series call
    A default call to  eval() evaluates the expression in the environment
    from which eval() was called (i.e. the chart_Series environment in this
    case). If an object in the expression does not exist in the chart_Series
    environment, or in an environment attached to the search path, the
    object will not be found.
    
    This is because variable lookup will progress from the chart_Series
    environment, to its parent environment (i.e. the environment
    chart_Series was *created* in), the quantmod namespace. From there,
    lookup will continue through the search path. If the object is not in
    an environment on the search path, lookup will fail.
    
    A simple example to illustrate the problem is calling chart_Series in a
    function and using an object defined in the function in the TA string.
    
    foo <- function() {
      bar <- .xts(10 + cumsum(rnorm(100)), 1:100)
      chart_Series(bar, TA='add_TA(SMA(bar, 50), on=1, col=2)')
    }
    foo()  # Error in inherits(x, "xts") : object 'bar' not found
    
    Also remove if-statement, since both the true and false sections were
    the same.
    
    Fixes #130.

--- R/chart_Series.R
+++ R/chart_Series.R
@@ -371,14 +371,11 @@ chart_Series <- function(x,
 
   # handle TA="add_Vo()" as we would interactively FIXME: allow TA=NULL to work
   if(!is.null(TA) && nchar(TA) > 0) {
-  TA <- parse(text=TA, srcfile=NULL)
-  for( ta in 1:length(TA)) {
-    if(length(TA[ta][[1]][-1]) > 0) {
-    cs <- eval(TA[ta])
-    } else {
-    cs <- eval(TA[ta])
+    TA <- parse(text=TA, srcfile=NULL)
+    for(ta in seq_along(TA)) {
+      # evaluate TA in environment from which chart_Series was called
+      cs <- eval(TA[ta], envir = parent.frame())
     }
-  }
   }
   assign(".chob", cs, .plotEnv)
   cs
