398c360a76bc9239559843f9649c96094716acb9
i <isomorphisms@sdf.org>
2017-06-23 19:20:35 -0400

    put back josh's tests
    accidentally deleted some of  josh's tests

--- tests/tests.R
+++ tests/tests.R
@@ -1,3 +1,74 @@
+
+
+
+
+
+# Call as.zoo before quantmod is loaded and registers its S3 method
+
+dc <- c("2015-01-01", "2016-01-01", "2017-01-01")
+
+dd <- as.Date(dc)
+
+
+
+
+f <- data.frame(a = 1:3)
+
+r <- f
+
+rownames(r) <- dc
+
+
+
+
+zz.f.date <- zoo::as.zoo(f, order.by = dd)
+
+zz.f.char <- zoo::as.zoo(f, order.by = dc)
+
+zz.f <- zoo::as.zoo(f)
+
+
+
+zz.r.date <- zoo::as.zoo(r, order.by = dd)
+
+
+zz.r.char <- zoo::as.zoo(r, order.by = dc)
+
+zz.r <- zoo::as.zoo(r)
+
+
+
+library(quantmod)
+
+
+
+### quantmod:::as.zoo.data.frame
+
+
+
+# should be the same as zoo:::as.zoo.data.frame when order.by is provided
+
+stopifnot(identical(zz.f.char, as.zoo(f, order.by = dc)))
+
+stopifnot(identical(zz.f.date, as.zoo(f, order.by = dd)))
+
+stopifnot(identical(zz.r.char, as.zoo(r, order.by = dc)))
+
+stopifnot(identical(zz.r.date, as.zoo(r, order.by = dd)))
+
+
+
+# should not throw a warning
+
+op.warn <- getOption("warn")
+
+options(warn = 2)
+
+quantmod::getSymbols("SPY", src = "google", from = Sys.Date() - 10)
+
+options(warn = op.warn)
+
+
 #IEX tests
 
 structure(list(symbol = c("VXX", "SNAP"), price = c(12.785, 17.27),
