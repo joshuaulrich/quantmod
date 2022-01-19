d4fca2b6df9c42258df662264229b5fada2c4e24
Ethan Smith <24379655+ethanbsmith@users.noreply.github.com>
2022-01-19 15:49:04 -0700

    added as.data.frame test

--- tests/test_getFinancials.R
+++ tests/test_getFinancials.R
@@ -9,6 +9,10 @@ if (nzchar(apikey)) {
   aapl <- getFinancials("AAPL", src = "tiingo", api.key = apikey, 
     auto.assign = FALSE)
   stopifnot(inherits(aapl, "financials"))
+  aapl.df <- as.data.frame(aapl)
+  stopifnot(is.data.frame(aapl.df))
+  #conversion to df shoudl test unpacing nested elements
+  stopifnot(names(aapl.df) == c("date", "entry", "period", "value", "type"))
 
   #test multisymbol path with bad symbols
   retsym <- getFinancials("AAPL;BA;UNKNOWNSYMBOL", src = "tiingo", 
