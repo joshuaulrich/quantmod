9f9b7b2901caa6a85571165bd629271997c9217f
Ethan Smith <24379655+ethanbsmith@users.noreply.github.com>
2023-05-18 18:27:42 -0600

    minor cleanup

--- R/getQuote.R
+++ R/getQuote.R
@@ -45,9 +45,9 @@ function(Symbols,src='yahoo',what, ...) {
     #get a crumb so that downstream callers dont have to handle invalid sessions.
     #this is a network hop, but very lightweight payload
     n <- if (unclass(Sys.time()) %% 1L >= 0.5) 1L else 2L
-    query.srv <- paste0("https://query", n, ".finance.yahoo.com/", "v1/test/getcrumb")
+    query.srv <- paste0("https://query", n, ".finance.yahoo.com/v1/test/getcrumb")
     r <- curl::curl_fetch_memory(query.srv, handle = ses$h)
-    if (r$status_code == 200) {
+    if ((r$status_code == 200) && (length(r$content) > 0)) {
       ses$crumb = rawToChar(r$content)
     } else {
       if (!force.new) ses <- .yahooSession(TRUE) else stop("Unbale to get yahoo crumb")
