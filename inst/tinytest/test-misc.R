0eabd0ce4b4925c033de44a5bfa03cc91e7c1f17
Joshua Ulrich <josh.m.ulrich@gmail.com>
2025-04-04 19:32:18 -0500

    Remove quantmod::as.zoo.data.frame() tests
    These tests no longer work as intended with 'tinytest' because quantmod
    is loaded and its as.zoo.data.frame() method is registered before the
    test suite is run. So we cannot call the zoo method before calling the
    quantmod method.
    
    Removing these tests isn't an issue because the quantmod method will be
    removed in a near-future version.

--- inst/tinytest/test-misc.R
+++ inst/tinytest/test-misc.R
@@ -2,32 +2,6 @@ av.key <- Sys.getenv("QUANTMOD_AV_API_KEY")
 tiingo.key <- Sys.getenv("QUANTMOD_TIINGO_API_KEY")
 test.web.endpoints <- Sys.getenv("QUANTMOD_TEST_WEB_ENDPOINTS")
 
-# Call as.zoo before quantmod is loaded and registers its S3 method
-dc <- c("2015-01-01", "2016-01-01", "2017-01-01")
-dd <- as.Date(dc)
-
-f <- data.frame(a = 1:3)
-r <- f
-rownames(r) <- dc
-
-zz.f.date <- zoo::as.zoo(f, order.by = dd)
-zz.f.char <- zoo::as.zoo(f, order.by = dc)
-zz.f <- zoo::as.zoo(f)
-
-zz.r.date <- zoo::as.zoo(r, order.by = dd)
-zz.r.char <- zoo::as.zoo(r, order.by = dc)
-zz.r <- zoo::as.zoo(r)
-
-library(quantmod)
-
-### quantmod:::as.zoo.data.frame
-
-# should be the same as zoo:::as.zoo.data.frame when order.by is provided
-stopifnot(identical(zz.f.char, as.zoo(f, order.by = dc)))
-stopifnot(identical(zz.f.date, as.zoo(f, order.by = dd)))
-stopifnot(identical(zz.r.char, as.zoo(r, order.by = dc)))
-stopifnot(identical(zz.r.date, as.zoo(r, order.by = dd)))
-
 if (nzchar(test.web.endpoints)) {
   # should throw an error
   errorKey <- "d116c846835e633aacedb1a31959dd2724cd67b8"
